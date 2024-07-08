5e380fd1510f2acf7a15a92a663ce8ec14f0777b
nguyenhuuhoangan2504@gmail.com <nguyenhuuhoangan2504@gmail.com>
2024-07-08 18:02:21 +0700

    test

--- scripts/get_files_from_docker.sh
+++ scripts/get_files_from_docker.sh
@@ -6,5 +6,5 @@ docker rm temp-wordpress
 #create a tempory container used to new pushed
 docker create --name temp-wordpress nhhan2504/wordpress:version1
 
-#Copy the new files from the container 
+#Copy the new files from the container
 docker cp temp-wordpress:/var/www/html/wp-admin ./wordpress-new/
