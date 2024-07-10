02dc6d374f17cac503f650be0fe864de2a7e9f1a
nhhandevops <nguyenhuuhoangan2504@gmail.com>
2024-07-10 10:48:31 +0700

    update

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Pull new changes in repo
-git clone https://github.com/nhhandevops/test-jenkins.git
+cd /home/ec2-user/ && git clone https://github.com/nhhandevops/test-jenkins.git
 cd /home/ec2-user/test-jenkins && git pull origin main -C /home/ec2-user
 #stop all container 
 cd /home/ec2-user/test-jenkins && docker-compose -f nginx-jenkins-docker-compose.yml down
