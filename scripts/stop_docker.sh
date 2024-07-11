eb3eefdffc68238fd5b8abcdd0201af41aa1e777
nguyenhuuhoangan2504@gmail.com <nguyenhuuhoangan2504@gmail.com>
2024-07-11 22:59:06 +0700

    update

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Pull new changes in repo
-cd /home/ec2-user/test-jenkins \
-    && git pull origin main \
-    && docker-compose -f nginx-jenkins-docker-compose.yml down
+
+git pull origin main
+docker-compose -f nginx-jenkins-docker-compose.yml down
